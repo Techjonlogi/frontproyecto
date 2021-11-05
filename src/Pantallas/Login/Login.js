a711572bb95d5a4fa87f4ea4f051b49a3e7b79a6
jonathan <jhoni65475@gmail.com>
2021-11-05 14:04:12 -0600

    Se termino el login junto a sus validaciones (falta conectarlo al api y agregar feedback)
    En este comit se realizo la validacion de los inputs de login, da algo de retroalimentacion, pero no como deberia, se agregara un mensaje mas adelante.

--- src/Pantallas/Login/Login.js
+++ src/Pantallas/Login/Login.js
@@ -1,4 +1,4 @@
-import react from "react";
+import react,{useState} from "react";
 import { Container, Form, Row, Col, Button } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.css';
 import imagenUsuario from "../../Imagenes/UsuarioImagen.svg"
@@ -8,23 +8,103 @@ import imagenFondo from"../../Pantallas/Login/Imagenes/LoginImagen.jpeg"
 
 
 const Login = () => {
+
+
+  const expresiones = {
+    usuarioExpresion: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+    nombreExpresion: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+    passwordExpresion: /^.{4,12}$/, // 4 a 12 digitos.
+    correoExpresion: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    telefonoExpresion: /^\d{7,14}$/ // 7 a 14 numeros.
+  }
+
+
+  const [usuario,cambiarUsuario] = useState({ nombreUsuario:'', valido: ''});
+  const [contrasenia,cambiarContrasenia] = useState({campoContrasenia:'', valido: ''});
+
+
+  const validacion =(valor)=>{
+      
+    if(expresiones.usuarioExpresion.test(usuario.nombreUsuario)){
+      console.log('input correcto')
+      cambiarUsuario({...usuario,nombreUsuario:valor ,valido:'true'});
+      const inputUsuario = document.getElementById('inputUsuario');
+      inputUsuario.style.borderColor ="#ced4da";
+    
+      
+    }else{
+      console.log('input incorrecto')
+      cambiarUsuario({...usuario,nombreUsuario:valor ,valido:'false'});
+      const inputUsuario = document.getElementById('inputUsuario');
+      inputUsuario.style.borderColor ="#FF0000";
+    
+    }
+
+
+}
+
+const validacionContrasenia =(valor)=>{
+      
+  if(expresiones.passwordExpresion.test(contrasenia.campoContrasenia)){
+    cambiarContrasenia({...contrasenia,campoContrasenia:valor ,valido:'true'});
+    const inputContrasenia = document.getElementById('inputContrasenia');
+    inputContrasenia.style.borderColor ="#ced4da";
+ 
+
+  }else{
+    console.log('input incorrecto')
+    cambiarContrasenia({...contrasenia,campoContrasenia:valor ,valido:'false'});
+    const inputContrasenia = document.getElementById('inputContrasenia');
+    inputContrasenia.style.borderColor ="#FF0000";
+  }
+
+}
+
+  const onChange = (e) =>{
+
+    validacion(e.target.value);
+
+  }
+
+  const onChangeContrasenia = (e) =>{
+    
+    validacionContrasenia(e.target.value);
+  
+    }
+
+    const HacerLogin =()=>{
+      if(contrasenia.valido==='true'){
+        if (usuario.valido ==='true'){
+          console.log(contrasenia.campoContrasenia,usuario.nombreUsuario);
+
+        }else{
+          console.log('Error con el usuario')
+        }
+
+      }else{console.log('error en la contrasenia')}
+
+
+    }
+  
+
+
   return (
     
       <Container className="text-center" id="ContenedorPrincipal" >
         <Row>
-          <Col lg={4} md={6} sm={12} className="text-center mt-5 p-3">
+          <Col lg={4} md={6} sm={12} className="text-center mt-5 p-5">
               <img className="icon-img" src= {imagenUsuario} alt="Icono usuario"></img>
             <Form>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Usuario</Form.Label>
-                <Form.Control  type="text" placeholder="Ingrese su usuario" />
+                <Form.Control id='inputUsuario' type="text" placeholder="Ingrese su usuario" name="usuario" value={usuario.nombreUsuario} onChange={onChange} />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Contraseña" />
+                <Form.Label>Contraseña</Form.Label>
+                <Form.Control id='inputContrasenia' type="password" placeholder="Contraseña" name="contrasenia" value={contrasenia.campoContrasenia} onChange={onChangeContrasenia}/>
               </Form.Group>
-              <Button variant="primary btn-block" type="submit">
+              <Button variant="primary btn-block p-2 mt-5"  onClick={HacerLogin}>
                 Ingresar
               </Button>
               <div className="text-center mt-3" >
