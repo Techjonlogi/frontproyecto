ea92dad4398960202d98b5bb6cda052a326c43e5
jonathan <jhoni65475@gmail.com>
2021-11-25 17:24:45 -0600

    Cambios en la estructura de Login y home, asi como agregando los componentes Header y Navbar
    Cambios en la estructura de Login y home, asi como agregando los componentes Header y Navbar

--- src/Pantallas/ComponentesVarios/NavBar.jsx
+++ src/Pantallas/ComponentesVarios/NavBar.jsx
@@ -5,49 +5,22 @@ import { Nav, Button, NavItem, Card } from "react-bootstrap";
 const NavBar = () => {
     return (
     <div>
-         <Nav
-        id="menuPrincipal"
-        activeKey="/home"
-        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-      >
-        <Nav.Item>
-          <Nav.Link href="/home">Logo</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/Login">Juegos</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">Conceptual</Nav.Link>
-        </Nav.Item>
-
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">Cine</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">Danza</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">Pintura</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">Musica</Nav.Link>
-        </Nav.Item>
-
-        <NavItem>
-          <div>
-            <a href="#">
-              <small className="reset">Login</small>
-            </a>
-          </div>
-        </NavItem>
-        <NavItem>
-          <div>
-            <a href="#">
-              <small className="reset ml-2">Registrate</small>
-            </a>
-          </div>
-        </NavItem>
-      </Nav>
+      <Nav justify variant="tabs" defaultActiveKey="/home">
+  <Nav.Item>
+    <Nav.Link href="/home">Active</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="/Login">Loooonger NavLink</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-2">Link</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-3" >
+      Disabled
+    </Nav.Link>
+  </Nav.Item>
+</Nav>
 
     </div>);
 };
--- src/Pantallas/Home/Home.js
+++ src/Pantallas/Home/Home.js
@@ -1,53 +1,13 @@
 import React from "react";
 import { Nav, Button, NavItem, Card } from "react-bootstrap";
 import "./Home.css";
+import NavBar from "../ComponentesVarios/NavBar";
 
 const Home = () => {
   return (
     <>
-      <Nav
-        id="menuPrincipal"
-        activeKey="/home"
-        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-      >
-        <Nav.Item>
-          <Nav.Link href="/home">Logo</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-1">Juegos</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">Conceptual</Nav.Link>
-        </Nav.Item>
-
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">Cine</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">Danza</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">Pintura</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">Musica</Nav.Link>
-        </Nav.Item>
-
-        <NavItem>
-          <div>
-            <a href="#">
-              <small className="reset">Login</small>
-            </a>
-          </div>
-        </NavItem>
-        <NavItem>
-          <div>
-            <a href="#">
-              <small className="reset ml-2">Registrate</small>
-            </a>
-          </div>
-        </NavItem>
-      </Nav>
+    <NavBar>
+    </NavBar>
 
       <Card style={{ width: "18rem" }}>
         <Card.Img variant="top" src="holder.js/100px180" alt="Aqui va la imagen de la publicacion"/>
