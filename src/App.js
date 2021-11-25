bcc3a37acd12b10487fa69b8051164e165827a91
jonathan <jhoni65475@gmail.com>
2021-11-25 16:32:58 -0600

    Agregando pagina de error 404
    se agrego la pagina 404

--- src/App.js
+++ src/App.js
@@ -8,14 +8,19 @@ import './App.css';
 
 import Login from './Pantallas/Login/Login'
 import Home from './Pantallas/Home/Home';
+import Error404 from './Pantallas/PaginaError/Error404';
+
 
 
 function App() {
   return (
    <BrowserRouter>
    <Routes>
-     <Route path="Login" element={<Login/>}></Route>
-     <Route path="Home" element={<Home/>}></Route>
+     <Route exact path="Login" element={<Login/>}></Route>
+     <Route exact path="Home" element={<Home/>}></Route>
+     <Route exact path="/" element={<Home/>}></Route>
+     <Route path="*" element={<Error404/>}></Route>
+     
      
    </Routes>
    </BrowserRouter>
---
+++ src/Pantallas/PaginaError/Error404.js
@@ -0,0 +1,19 @@
+import React,{useState} from "react";
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+
+
+
+
+const Error404 = () => {
+
+    return(<div>
+        <h1>
+            No le sabes a la pagina maistro
+        </h1>
+    </div>);
+};
+
+export default Error404;
