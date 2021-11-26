6d26702861344d157906a33a7f254613ac24fa5b
jonathan <jhoni65475@gmail.com>
2021-11-26 11:39:14 -0600

    Update Home.js

--- src/Pantallas/Home/Home.js
+++ src/Pantallas/Home/Home.js
@@ -5,21 +5,15 @@ import NavBar from "../ComponentesVarios/NavBar";
 const Home = () => {
   return (
     <>
-    <NavBar>
-    </NavBar>
-
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" alt="Aqui va la imagen de la publicacion"/>
-        <Card.Body>
-          <Card.Title>Titulo De Publicacion</Card.Title>
-          <Card.Text>
-            Aqui va la descripcion de la publicacion
-          </Card.Text>
-          <Button variant="primary">Ver publicacion</Button>
-        </Card.Body>
-      </Card>
+    <NavBar/>
     </>
   );
 };
+// cada componente va aqui
+// const DefinirComponente =(cosas)=> {
+//   switch
+
+// }
+//handleclick
 
 export default Home;
