41f0b07712c1e2854eb79fbf59ea673814efc6c0
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-11-27 13:54:42 -0600

    Se agrego ruta para la pagina UserRegistry. Se agrego imagen a UserRegistry. Configuracion basica de tamano.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 import Login from './Pantallas/Login/Login'
 import Home from './Pantallas/Home/Home';
 import Error404 from './Pantallas/PaginaError/Error404';
+import UserRegistry from './Pantallas/UserRegistry/UserRegistry';
 
 function App() {
   return (
    <BrowserRouter>
    <Routes>
      <Route exact path="Login" element={<Login/>}></Route>
+     <Route exact path="Register" element={<UserRegistry/>}></Route>
      <Route exact path="Home" element={<Home/>}></Route>
      <Route exact path="/" element={<Home/>}></Route>
      <Route path="*" element={<Error404/>}></Route>
--- src/Pantallas/UserRegistry/UserRegistry.js
+++ src/Pantallas/UserRegistry/UserRegistry.js
@@ -1,10 +1,19 @@
 import React from "react"
+import { Container, Form, Row, Col, Button } from "react-bootstrap";
+import imagenFondo from"../../Pantallas/Login/Imagenes/LoginImagen.jpeg"
 import 'bootstrap/dist/css/bootstrap.css';
+import './UserRegistry.css';
 
 const UserRegistry = () => {
     return (
-        <>
-        </>
+        <Container className="UserRegistryContent" fluid>
+            <Col className="formColumn">
+
+            </Col>
+            <Col className="imageColumn">
+                <img src={ imagenFondo } alt="" id="ImagenFondo" />
+            </Col>
+        </Container>
     );
 };
 
