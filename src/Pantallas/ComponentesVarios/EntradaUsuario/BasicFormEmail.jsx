d2a93f8680d5fda8aa19af4cb8f1fd974b0dd938
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-11-27 16:10:19 -0600

    Modificacion en configuracion de css de pagina UserRegistry.

--- src/Pantallas/ComponentesVarios/EntradaUsuario/BasicFormEmail.jsx
+++ src/Pantallas/ComponentesVarios/EntradaUsuario/BasicFormEmail.jsx
@@ -2,7 +2,7 @@ import { Form } from 'react-bootstrap';
 
 const BasicFormEmail = ( props ) => {
     return (
-        <Form.Group className="BasicFormEmailGroup" id={ props.groupId }>
+        <Form.Group className="BasicFormInputGroup" id={ props.groupId }>
             <Form.Label className="FormLabel" id={ props.labelId }>{ props.label }</Form.Label>
             <Form.Control className="FormControlInput" type="email" placeholder={ props.placeholder }></Form.Control>
         </Form.Group>
