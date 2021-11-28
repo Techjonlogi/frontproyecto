f63a57706f1edc6097bc71aa0ce175f8b386e956
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-11-27 20:34:37 -0600

    Modificaciones esteticas de archivo.

--- src/Pantallas/ComponentesVarios/NavBar.jsx
+++ src/Pantallas/ComponentesVarios/NavBar.jsx
@@ -1,31 +1,28 @@
-import { Nav} from "react-bootstrap";
+import { Nav } from "react-bootstrap";
 
 
 
-const NavBar = (idCategoria) => {
-
-  
-
+const NavBar = ( idCategoria ) => {
     return (
     <div>
       <Nav justify variant="tabs" defaultActiveKey="/home">
-  <Nav.Item>
-    <Nav.Link href="/home">Active</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link href="/login">Loooonger NavLink</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="link-2">Link</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="link-3" >
-      Disabled
-    </Nav.Link>
-  </Nav.Item>
-</Nav>
-
-    </div>);
+        <Nav.Item>
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/login">Loooonger NavLink</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-3" >
+            Disabled
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </div>
+    );
 };
 
 export default NavBar;
