eb109fd1227f66bf36e45b0c681a0ee84f0435c7
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-11-27 21:27:46 -0600

    Creacion e implementacion basica de barra de busqueda.

---
+++ src/Pantallas/ComponentesVarios/EntradaUsuario/BasicSearchBar.jsx
@@ -0,0 +1,11 @@
+import { Form } from "react-bootstrap";
+
+const SearchBar = () => {
+    return (
+        <Form.Group>
+            <Form.Control></Form.Control>
+        </Form.Group>
+    );
+};
+
+export default SearchBar;
