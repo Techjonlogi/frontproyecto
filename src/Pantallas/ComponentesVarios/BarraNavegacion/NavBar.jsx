30749c08c4af884fe0bbb180e56dc1e0581828a2
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-11-29 09:19:45 -0600

    Limpieza general de archivo.

--- src/Pantallas/ComponentesVarios/BarraNavegacion/NavBar.jsx
+++ src/Pantallas/ComponentesVarios/BarraNavegacion/NavBar.jsx
@@ -18,25 +18,6 @@ const NavBar = ( idCategoria ) => {
           </Col>
         </Row>
       </Navbar>
-
-    // <div className="BarraNavegacion">
-    //   <Nav justify variant="tabs" defaultActiveKey="/home">
-    //     <Nav.Item>
-    //       <Nav.Link href="/home">Active</Nav.Link>
-    //     </Nav.Item>
-    //     <Nav.Item>
-    //       <Nav.Link href="/login">Loooonger NavLink</Nav.Link>
-    //     </Nav.Item>
-    //     <Nav.Item>
-    //       <Nav.Link eventKey="link-2">Link</Nav.Link>
-    //     </Nav.Item>
-    //     <Nav.Item>
-    //       <Nav.Link eventKey="link-3" >
-    //         Disabled
-    //       </Nav.Link>
-    //     </Nav.Item>
-    //   </Nav>
-    // </div>
     );
 };
 
