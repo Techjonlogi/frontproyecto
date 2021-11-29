f7536086eaf3051444ecc1a4b1d762027bfe10d9
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-11-29 16:29:37 -0600

    Implementacion de vista general de Informacion de usuario en pantalla PerfilUsuario.

--- src/Pantallas/ComponentesVarios/PerfilUsuario/InformacionUsuario.jsx
+++ src/Pantallas/ComponentesVarios/PerfilUsuario/InformacionUsuario.jsx
@@ -1,7 +1,46 @@
-const InformacionUsuario = () => {
+import PropTypes from "prop-types";
+import { Container, Form } from "react-bootstrap";
+import ImagenPerfil from "../../../Imagenes/UsuarioImagen.svg";
+
+const InformacionUsuario = ( props ) => {
     return (
-        <></>
+        <Container className="Capsula">
+            <Container className="Capsula" id="InformacionUsuarioCabeza">
+                <img className="ProfileImage" src={ ImagenPerfil } alt="ImagenPerfil" ></img>
+                <Form>
+                    <Form.Label className="FormLabel" id="Username">{ props.nombreUsuario }</Form.Label>
+                </Form>
+            </Container>
+            <Container className="Capsula" id="InformacionUsuarioCuerpo">
+                <dl>
+                    <dt>Nombres:</dt>
+                    <dd>{ props.nombres }</dd>
+                    <dt>Apellidos:</dt>
+                    <dd>{ props.apellidos }</dd>
+                    <dt>Correo:</dt>
+                    <dd>{ props.correo }</dd>
+                    <dt>Se unio en:</dt>
+                    <dd>{ props.unioEn }</dd>
+                </dl>
+            </Container>
+        </Container>
     );
 };
+
+InformacionUsuario.defaultProps = {
+    nombreUsuario: "Nombre de Usuario",
+    nombres: "Nombres",
+    apellidos: "Apellidos",
+    correo: "Correo",
+    unioEn: "2020-12-01"
+}
+
+InformacionUsuario.propTypes = {
+    nombreUsuario: PropTypes.string,
+    nombres: PropTypes.string,
+    apellidos: PropTypes.string,
+    correo: PropTypes.string,
+    unioEn: PropTypes.string
+}
 
 export default InformacionUsuario;
--- src/Pantallas/PerfilUsuario/PerfilUsuario.js
+++ src/Pantallas/PerfilUsuario/PerfilUsuario.js
@@ -1,12 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './PerfilUsuario.css';
 
-import NavBar from '../ComponentesVarios/BarraNavegacion/NavBar';
+import { Container, Row, Col } from "react-bootstrap";
+import NavBar from "../ComponentesVarios/BarraNavegacion/NavBar";
+import InformacionUsuario from '../ComponentesVarios/PerfilUsuario/InformacionUsuario';
 
 const PerfilUsuario = () => {
     return (
         <>
             <NavBar />
+            <Container className="PerfilContent" fluid>
+                <Row className="BodyRow">
+                    <Col id="EmptyLeftColumn" sm={ 2 }></Col>
+                    <Col id="UserInfoColumn" sm={ 3 }>
+                        <InformacionUsuario />
+                    </Col>
+                    <Col id="UserPublicationsColumn" sm={ 5 }></Col>
+                    <Col id="EmptyRightColumn" sm={ 2 }></Col>
+                </Row>
+            </Container>
         </>
     );
 };
