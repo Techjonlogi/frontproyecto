f60362da58fcfcb947d3c3dff329bf28de3b218f
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-11-29 13:30:31 -0600

    Se agrego la componente de CrearPublicacion a las rutas de la aplicacion.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Login from './Pantallas/Login/Login'
 import Home from './Pantallas/Home/Home';
 import Error404 from './Pantallas/PaginaError/Error404';
+import CrearPublicacionComponent from './Pantallas/CrearPublicacion/CrearPublicacion';
 import UserRegistry from './Pantallas/UserRegistry/UserRegistry';
 
 
@@ -15,8 +16,8 @@ function App() {
      <Route exact path="Login" element={<Login/>}></Route>
      <Route exact path="Register" element={<UserRegistry/>}></Route>
      <Route exact path="Home" element={<Home/>}></Route>
+     <Route exact path="Publicar" element={ <CrearPublicacionComponent /> }></Route>
      <Route exact path="/" element={<Home/>}></Route>
-    
      <Route path="*" element={<Error404/>}></Route>
      
      
