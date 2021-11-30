696a3f7b47579b66a13def3c5b5f3a4f512496d8
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-11-30 16:34:11 -0600

    Eliminacion de metodos y llamadas innecesarias. Se hara la validacion de las entradas con Formik y Yup.

--- src/Pantallas/UserRegistry/UserRegistry.js
+++ src/Pantallas/UserRegistry/UserRegistry.js
@@ -20,10 +20,10 @@ const UserRegistry = () => {
                 <Col className="formColumn">
                     <Form id="UserInputForm">
                         <Form.Label id="etiquetaPagina">Registro de Usuario</Form.Label>
-                        <BasicFormInput labelId="etiquetaNombres" label="" placeholder="Nombres" />
-                        <BasicFormInput labelId="etiquetaApellidos" label="" placeholder="Apellidos" />
+                        <BasicFormInput labelId="etiquetaNombres" controlId="controlNombres" label="" placeholder="Nombres"/>
+                        <BasicFormInput labelId="etiquetaApellidos" controlId="controlApellidos" label="" placeholder="Apellidos" />
                         <BasicDropdownButton dropdownId="BotonDropdown" title="Tipo de Usuario" items={ [ 'Artista', 'Buscador de Talento' ] } />
-                        <BasicFormInput labelId="etiquetaNombreUsuario" label="" placeholder="Nombre de Usuario" />
+                        <BasicFormInput labelId="etiquetaNombreUsuario" controlId="controlNombreUsuario" label="" placeholder="Nombre de Usuario" />
                         <BasicFormEmail labelId="etiquetaEmail" label="" placeholder="Correo Electrónico"/>
                         <BasicFormPassword labelId="etiquetaContrasena" label="" placeholder="Contraseña"/>
                         <BasicButton buttonId="BotonRegistro" content="Crear Cuenta" />
