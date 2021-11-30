6b460db3d8f673f2ce7fa2850d63f044deb121bd
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-11-30 17:01:19 -0600

    Se instalo Formik para la validacion de entradas del usuario. Implementacion inicial de formik en pantalla UserRegistry.

--- src/Pantallas/UserRegistry/UserRegistry.js
+++ src/Pantallas/UserRegistry/UserRegistry.js
@@ -1,7 +1,7 @@
-import React from "react"
 import 'bootstrap/dist/css/bootstrap.css';
 import './UserRegistry.css';
 
+import { Formik } from "formik";
 import { Container, Form, Row, Col } from "react-bootstrap";
 import ImagenFondo from"../../Pantallas/Login/Imagenes/LoginImagen.jpeg";
 import BasicFormEmail from "../ComponentesVarios/EntradaUsuario/BasicFormEmail";
@@ -12,25 +12,39 @@ import BasicButton from "../ComponentesVarios/Botones/BasicButton";
 
 const UserRegistry = () => {
     return (
-        <Container className="UserRegistryContent" fluid>
-            <Row>
-                <Col className="imageColumn">
-                    <img src={ ImagenFondo } alt="" id="ImagenFondo" />
-                </Col>
-                <Col className="formColumn">
-                    <Form id="UserInputForm">
-                        <Form.Label id="etiquetaPagina">Registro de Usuario</Form.Label>
-                        <BasicFormInput labelId="etiquetaNombres" controlId="controlNombres" label="" placeholder="Nombres"/>
-                        <BasicFormInput labelId="etiquetaApellidos" controlId="controlApellidos" label="" placeholder="Apellidos" />
-                        <BasicDropdownButton dropdownId="BotonDropdown" title="Tipo de Usuario" items={ [ 'Artista', 'Buscador de Talento' ] } />
-                        <BasicFormInput labelId="etiquetaNombreUsuario" controlId="controlNombreUsuario" label="" placeholder="Nombre de Usuario" />
-                        <BasicFormEmail labelId="etiquetaEmail" label="" placeholder="Correo Electrónico"/>
-                        <BasicFormPassword labelId="etiquetaContrasena" label="" placeholder="Contraseña"/>
-                        <BasicButton buttonId="BotonRegistro" content="Crear Cuenta" />
-                    </Form>
-                </Col>
-            </Row>
-        </Container>
+        <Formik
+            initialValues= { {
+                nombres: '',
+                apellidos: '',
+                nombreUsuario: '',
+                correo: '',
+                contrasena: '',
+                confirmarContrasena: ''
+            } }
+        >
+            { formik => (
+                <Container className="UserRegistryContent" fluid>
+                    { console.log(formik) }
+                <Row>
+                    <Col className="imageColumn">
+                        <img src={ ImagenFondo } alt="" id="ImagenFondo" />
+                    </Col>
+                    <Col className="formColumn">
+                        <Form id="UserInputForm">
+                            <Form.Label id="etiquetaPagina">Registro de Usuario</Form.Label>
+                            <BasicFormInput labelId="etiquetaNombres" controlId="controlNombres" label="" placeholder="Nombres"/>
+                            <BasicFormInput labelId="etiquetaApellidos" controlId="controlApellidos" label="" placeholder="Apellidos" />
+                            <BasicDropdownButton dropdownId="BotonDropdown" title="Tipo de Usuario" items={ [ 'Artista', 'Buscador de Talento' ] } />
+                            <BasicFormInput labelId="etiquetaNombreUsuario" controlId="controlNombreUsuario" label="" placeholder="Nombre de Usuario" />
+                            <BasicFormEmail labelId="etiquetaEmail" label="" placeholder="Correo Electrónico"/>
+                            <BasicFormPassword labelId="etiquetaContrasena" label="" placeholder="Contraseña"/>
+                            <BasicButton buttonId="BotonRegistro" content="Crear Cuenta" />
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
+            ) }
+        </Formik>
     );
 };
 
