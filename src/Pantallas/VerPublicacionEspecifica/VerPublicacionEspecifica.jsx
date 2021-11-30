f5e46b9a13584622384a512040039ccc9dc84d63
jonathan <jhoni65475@gmail.com>
2021-11-29 20:30:18 -0600

    Agregando pantalla para ver publicacion
    Agregando pantalla para ver publicacion

---
+++ src/Pantallas/VerPublicacionEspecifica/VerPublicacionEspecifica.jsx
@@ -0,0 +1,60 @@
+import {Button,Row,Col,Form,Table} from "react-bootstrap";
+import React from "react";
+import "./VerPublicacion.css"
+
+
+const ComponenteVerpublicacion = () => {
+    return(
+<>
+<section>
+    <Row>
+        <Col>
+        <img></img>
+        <Form>
+            <Form.Group>
+                <Form.Label>Nombre</Form.Label>
+                <Form.Control type= "text"></Form.Control>
+                <Form.Label>Informacion General</Form.Label>
+                <Form.Control type= "text"></Form.Control>
+                <Form.Label>Informacion Publicacion</Form.Label>
+                <Form.Control type= "text"></Form.Control>
+
+            </Form.Group>
+        </Form>
+        </Col>
+        
+        <Col>
+        <img></img>
+        </Col>
+       
+    </Row>
+</section>
+<section className="seccionComentarios">
+    <Row className="divTablaComentarios">
+    <Table striped bordered hover className="tablaComentarios">
+            <thead>
+                <tr>
+                    <th>usuario</th>
+                    <th>Comentario</th>
+                    <th>Opciones</th>
+                    
+                </tr>
+            </thead>
+
+        </Table>
+        <Form className="formComentarios">
+        <Form.Control className="imputComentario" type="text" placeholder ="ingrese su comentario"></Form.Control>
+        <Row className="wrapper">
+        <Button className="btnPublicar">Comentar</Button>
+        </Row>
+        
+        </Form>
+    </Row>
+</section>
+</>
+
+    );
+
+}
+
+export default ComponenteVerpublicacion;
