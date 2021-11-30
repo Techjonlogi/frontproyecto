d6f8f244613cb1f762f3b85dce676825f015a06d
jonathan <jhoni65475@gmail.com>
2021-11-29 22:48:54 -0600

    Corrigiendo errores menores.
    Corrigiendo errores menores.

--- src/Pantallas/EditarPublicacion/EditarPublicacion.jsx
+++ src/Pantallas/EditarPublicacion/EditarPublicacion.jsx
@@ -10,15 +10,12 @@ const EditarPublicacion =()=>{
         <section className="seccionPublicacion">
             <Row>
                 <Col>
-                <img></img>
                 <Form className="formInformacion">
                     <Form.Group>
                         <Form.Label className="basicLabel">Nombre</Form.Label>
-                        <Form.Control type= "text"  className="basicInput"></Form.Control>
-                        <Form.Label className="basicLabel" >Informacion General</Form.Label>
-                        <Form.Control type= "text"  className="basicInput"></Form.Control>
-                        <Form.Label className="basicLabel">Informacion Publicacion</Form.Label>
-                        <Form.Control type= "text"  className="basicInput"></Form.Control>
+                        <Form.Control type= "text"  className="basicInput" placeholder="Ingrese el nuevo nombre..."></Form.Control>
+                        <Form.Label className="basicLabel" >Descripcion</Form.Label>
+                        <Form.Control type= "text"  className="basicInput" placeholder = "Ingrese nueva descripcion..."></Form.Control>
                         <Form.Label className="basicLabel">Nuevo Archivo</Form.Label>
                         <Form.Control className = "inputImagen" type="file" accept=".jpg,.png" />
                         <Button className="btnPublicar">Guardar</Button>
