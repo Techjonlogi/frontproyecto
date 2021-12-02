8a7883a0b4d1288af71b09be2314c439add283a6
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-12-01 20:39:47 -0600

    Implementacion general de estructura de pantalla CrearPublicacion.

--- src/Pantallas/CrearPublicacion/CrearPublicacion.jsx
+++ src/Pantallas/CrearPublicacion/CrearPublicacion.jsx
@@ -1,46 +1,76 @@
-import { FloatingLabel, Form, Figure, Button, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 import "./CrearPublicacion.css";
 
-const CrearPublicacionComponent = (idUsuario) => {
+import { Formik } from "formik";
+import { Container, FloatingLabel, Form, Figure, Button, Col, Row } from "react-bootstrap";
+import NavBar from "../ComponentesVarios/BarraNavegacion/NavBar";
+
+const CrearPublicacionComponent = () => {
   return (
-    <container className="ContenedorCrearPublicacion">
-      <Col xs={ 5 }>
-        <Form.Group controlId="formFile" className="mb-3" accept="image/*">
-          <Form.Label className="imputPublicacion">Seleccione el archivo a compartir</Form.Label>
-          <Form.Control type="file" accept=".jpg,.png" />
-        </Form.Group>
+    <Formik
+      initialValues= { {
+        nombre_publicacion: '',
+        descripcion_publicacion: '',
+        categoria: ''
+      } }
+    >
+      { formik => (
+        <>
+          <NavBar />
+          <Container className="CreatePublicationContent" fluid>
+            <Row className="BodyRow">
+              <Col id="EmptyLeftColumn" sm={ 3 }></Col>
+              <Col id="CreatePublicationColumn" sm={ 6 }>
+                <Container id="FormContainer">
+                  <Form>
+                    <Form.Label>Crear Publicación</Form.Label>
+                  </Form>
+                </Container>
+              </Col>
+              <Col id="EmptyRightColumn" sm={ 3 }></Col>
+            </Row>
+          </Container>
+        </>
+      ) }  
+    </Formik>
+    // <container className="ContenedorCrearPublicacion">
+    //   <Col xs={ 5 }>
+    //     <Form.Group controlId="formFile" className="mb-3" accept="image/*">
+    //       <Form.Label className="imputPublicacion">Seleccione el archivo a compartir</Form.Label>
+    //       <Form.Control type="file" accept=".jpg,.png" />
+    //     </Form.Group>
    
-        <Figure>
-          <Figure.Image
-            width={171}
-            height={180}
-            alt="171x180"
-            src="{this.formFile.file}"
-          />
-          <Figure.Caption></Figure.Caption>
-        </Figure>
+    //     <Figure>
+    //       <Figure.Image
+    //         width={171}
+    //         height={180}
+    //         alt="171x180"
+    //         src="{this.formFile.file}"
+    //       />
+    //       <Figure.Caption></Figure.Caption>
+    //     </Figure>
    
       
       
-      <Form>
-        <Form.Group className="mb-3">
-          <Form.Label className="imputPublicacion">Nombre Publicacion</Form.Label>
-          <Form.Control
-            id="inputNombre"
-            type="text"
-            placeholder="Ingrese el Nombre de su publicacion"/>
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label className="imputPublicacion">Nombre Publicacion</Form.Label>
-          <Form.Control
-            id="inputdescripcion"
-            type="text"
-            placeholder="Ingrese la descripcion de su publicacion"/>
-        </Form.Group>
-      </Form>
-      </Col>
-      <Button>Crear Publicacion</Button>
-      </container>
+    //   <Form>
+    //     <Form.Group className="mb-3">
+    //       <Form.Label className="imputPublicacion">Nombre Publicacion</Form.Label>
+    //       <Form.Control
+    //         id="inputNombre"
+    //         type="text"
+    //         placeholder="Ingrese el Nombre de su publicacion"/>
+    //     </Form.Group>
+    //     <Form.Group className="mb-3">
+    //       <Form.Label className="imputPublicacion">Nombre Publicacion</Form.Label>
+    //       <Form.Control
+    //         id="inputdescripcion"
+    //         type="text"
+    //         placeholder="Ingrese la descripcion de su publicacion"/>
+    //     </Form.Group>
+    //   </Form>
+    //   </Col>
+    //   <Button>Crear Publicacion</Button>
+    //   </container>
   );
 };
 
