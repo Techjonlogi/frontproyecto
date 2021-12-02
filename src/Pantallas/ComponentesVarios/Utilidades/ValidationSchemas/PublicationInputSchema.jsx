583f9bfa3c6a49c3831c30f02fc436259c057dbc
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-12-02 12:55:03 -0600

    Se agrego validacion a los campos de la pantalla CrearPublicacion.

---
+++ src/Pantallas/ComponentesVarios/Utilidades/ValidationSchemas/PublicationInputSchema.jsx
@@ -0,0 +1,22 @@
+import * as Yup from 'yup';
+import Expresiones from '../Regex';
+
+const PublicationinputSchema = Yup.object( {
+    nombre_publicacion: Yup.string()
+        .min( 3, 'Debe tener por lo menos 3 caracteres.' )
+        .max( 40, 'Debe tener 40 caracteres o menos.')
+        .matches( Expresiones.nombreExpresion, 'Nombres inválidos' )
+        .required( 'Por favor introduzca el nombre de su publicación' ),
+    descripcion_publicacion: Yup.string()
+        .min( 3, 'Debe tener por lo menos 3 caracteres.' )
+        .max( 200, 'Debe tener 200 caracteres o menos.')
+        .matches( Expresiones.nombreExpresion, 'Nombres inválidos' )
+        .required( 'Por favor introduzca una descripción' ),
+    categoria: Yup.number( 'Por favor seleccione una categoria' )
+        .required( 'Por favor seleccione una categoria' )
+        .min( 0 )
+        .max( 20 )
+        .integer()
+} );
+
+export default PublicationinputSchema;
--- src/Pantallas/CrearPublicacion/CrearPublicacion.js
+++ src/Pantallas/CrearPublicacion/CrearPublicacion.js
@@ -2,7 +2,8 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./CrearPublicacion.css";
 
 import { Formik } from "formik";
-import { Container, FloatingLabel, Form, Figure, Button, Col, Row } from "react-bootstrap";
+import { Container, Form, Figure, Col, Row } from "react-bootstrap";
+import PublicationinputSchema from "../ComponentesVarios/Utilidades/ValidationSchemas/PublicationInputSchema";
 import NavBar from "../ComponentesVarios/BarraNavegacion/NavBar";
 import BasicFormFileInput from "../ComponentesVarios/EntradaUsuario/BasicFormFileInput";
 import BasicFormInput from "../ComponentesVarios/EntradaUsuario/BasicFormInput";
@@ -18,6 +19,7 @@ const CrearPublicacion = () => {
         descripcion_publicacion: '',
         categoria: ''
       } }
+      validationSchema= { PublicationinputSchema }
     >
       { formik => (
         <>
@@ -28,7 +30,7 @@ const CrearPublicacion = () => {
               <Col id="EmptyLeftColumn" sm={ 3 }></Col>
               <Col id="CreatePublicationColumn" sm={ 6 }>
                 <Container id="FormContainer">
-                  <Form id="CreatePublicationForm">
+                  <Form id="CreatePublicationForm" onSubmit={ formik.handleSubmit }>
                     <Form.Label id="PublicationContentLabel">Crear Publicación</Form.Label>
                     <BasicFormFileInput />
                     <BasicFormInput labelId="etiquetaNombrePublicacion" label="Nombre Publicación" controlId="controlNombrePublicacion" name="nombre_publicacion"/>
@@ -44,44 +46,6 @@ const CrearPublicacion = () => {
         </>
       ) }  
     </Formik>
-    // <container className="ContenedorCrearPublicacion">
-    //   <Col xs={ 5 }>
-    //     <Form.Group controlId="formFile" className="mb-3" accept="image/*">
-    //       <Form.Label className="imputPublicacion">Seleccione el archivo a compartir</Form.Label>
-    //       <Form.Control type="file" accept=".jpg,.png" />
-    //     </Form.Group>
-   
-    //     <Figure>
-    //       <Figure.Image
-    //         width={171}
-    //         height={180}
-    //         alt="171x180"
-    //         src="{this.formFile.file}"
-    //       />
-    //       <Figure.Caption></Figure.Caption>
-    //     </Figure>
-   
-      
-      
-    //   <Form>
-    //     <Form.Group className="mb-3">
-    //       <Form.Label className="imputPublicacion">Nombre Publicacion</Form.Label>
-    //       <Form.Control
-    //         id="inputNombre"
-    //         type="text"
-    //         placeholder="Ingrese el Nombre de su publicacion"/>
-    //     </Form.Group>
-    //     <Form.Group className="mb-3">
-    //       <Form.Label className="imputPublicacion">Nombre Publicacion</Form.Label>
-    //       <Form.Control
-    //         id="inputdescripcion"
-    //         type="text"
-    //         placeholder="Ingrese la descripcion de su publicacion"/>
-    //     </Form.Group>
-    //   </Form>
-    //   </Col>
-    //   <Button>Crear Publicacion</Button>
-    //   </container>
   );
 };
 
