2383c44f5775aae0785f17017512b47d11ca5443
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-12-01 20:42:23 -0600

    Creacion e implementacion base de componente BasicFormFileInput.jsx.

---
+++ src/Pantallas/ComponentesVarios/EntradaUsuario/BasicFormFileInput.jsx
@@ -0,0 +1,11 @@
+import { Form } from "react-bootstrap";
+
+const BasicFormFileInput = () => {
+    return (
+        <Form.Group>
+            <Form.Label>Selecciona el archivo deseado</Form.Label>
+        </Form.Group>
+    );
+};
+
+export default BasicFormFileInput;
