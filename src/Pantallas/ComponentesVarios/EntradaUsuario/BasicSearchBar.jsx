2f0766917c808d5a2999b7bd6bd318630b4388a3
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-12-03 13:43:22 -0600

    Agregue funcionalidad de formik al componente NavBar.

--- src/Pantallas/ComponentesVarios/EntradaUsuario/BasicSearchBar.jsx
+++ src/Pantallas/ComponentesVarios/EntradaUsuario/BasicSearchBar.jsx
@@ -1,12 +1,19 @@
 import { Form, InputGroup } from "react-bootstrap";
+import { useField } from "formik";
 import BasicButton from "../Botones/BasicButton";
 import PropTypes from "prop-types";
 
 const SearchBar = ( props ) => {
+    const [ field, meta ] = useField( props );
     return (
         <Form.Group className="BasicSearchInputGroup">
             <InputGroup>
-                <Form.Control className="FormControlInput" id="SearchBar" type="text" placeholder={ props.placeholder }></Form.Control>
+                <Form.Control 
+                    className={ `FormControlInput ${ meta.touched && meta.error && 'is-invalid' }` } 
+                    id="SearchBar" type="text" placeholder={ props.placeholder }
+                    { ...field }
+                >
+                </Form.Control>
                 <BasicButton buttonId="BotonBuscar" content="Buscar" />
             </InputGroup>
         </Form.Group>
