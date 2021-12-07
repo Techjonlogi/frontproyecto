5955f9ec18efd601baf1d62b4b9d6e3f26eed725
jonathan <jhoni65475@gmail.com>
2021-12-06 21:59:43 -0600

    corrigiendo errores al obtener y juntar multimedia de las publicaciones
    corrigiendo errores al obtener y juntar multimedia de las publicaciones

--- src/Pantallas/Home/Home.js
+++ src/Pantallas/Home/Home.js
@@ -9,24 +9,24 @@ import NavBar from "../ComponentesVarios/BarraNavegacion/NavBar";
 
 const Home = () => { 
 let publicaciones =[]
+let publicacionesConMultimedia =[]
   // eslint-disable-next-line react-hooks/exhaustive-deps
   React.useEffect(async()=>{
     await Api.get(Endpoints.publicaciones).then( (res) =>{
       publicaciones = res.data;
-      console.log(publicaciones)
     }).catch(( e ) =>
     alert("ocurrio algun error al recuperar las publicaciones")
     );
 
     await publicaciones.forEach(publicacion =>(
       Api.get(Endpoints.multimedia +"/"+ publicacion.clave_publicacion +Endpoints.obtenerMultimediaEspecifica).then((res)=>{
-        publicaciones.multimedia = res.data.multimedia;
+        publicacion.multimedia = res.data.multimedia;
+        publicacionesConMultimedia.push(publicacion);
+        console.log(publicacionesConMultimedia);
         
       }).catch((e) =>
       alert("Ocurrio un error al cargar las imagenes")
       )
-
-
     ))
 
 
