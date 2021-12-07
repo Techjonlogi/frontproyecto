4475dbceb94ee6dfb82b3814bf82156fb8906e40
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-12-06 20:54:09 -0600

    Se agregaron variables de estado al componente Busqueda.

--- src/Pantallas/Busqueda/Busqueda.js
+++ src/Pantallas/Busqueda/Busqueda.js
@@ -1,7 +1,20 @@
+import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import Api from "../ComponentesVarios/Utilidades/Api/Api";
+import ConfigNoAuth from "../ComponentesVarios/Utilidades/Api/Configurations/ConfigNoAuth";
+import Endpoints from "../ComponentesVarios/Utilidades/Api/ApiEndpoints";
 import NavBar from "../ComponentesVarios/BarraNavegacion/NavBar";
 
 const Busqueda = () => {
+    const [ items, setItems ] = useState( [] ); 
+    const [ loadedItems, setLoaded ] = useState( false );
+
+    useEffect( () => {
+        await Api.get(
+            
+        ).then().catch()
+    } )
+
     return (
         <>
             <NavBar />
