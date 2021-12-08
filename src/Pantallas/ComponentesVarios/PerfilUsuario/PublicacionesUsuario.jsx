594a0e0f2c0fb95bd0103edce116f1f3c19ecf72
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-12-08 16:41:44 -0600

    Implmentacion de mapeo de publicaciones del usuario para que sean mostradas en el perfil del usuario.

--- src/Pantallas/ComponentesVarios/PerfilUsuario/PublicacionesUsuario.jsx
+++ src/Pantallas/ComponentesVarios/PerfilUsuario/PublicacionesUsuario.jsx
@@ -1,7 +1,33 @@
-const PublicacionesUsuario = () => {
+import { Container, Form, Nav } from "react-bootstrap";
+import PropTypes from "prop-types";
+
+const PublicacionesUsuario = ( props ) => {
     return (
-        <></>
+        <>
+            <Container className="PublicacionesUsuario">
+                <Container className="PublicacionesTitle">
+                    <Form.Label>Publicaciones del Usuario:</Form.Label>
+                </Container>
+                <Container className="Publicaciones">
+                    { props.publicaciones.map( ( publicacion, index ) => (
+                        <Container className="Publicacion">
+                            <Nav.Link href="/Home">
+                                <img className="PublicacionImage" src={ publicacion.multimedia } alt="imagen"></img>
+                            </Nav.Link>
+                        </Container>
+                    ) ) }
+                </Container>
+            </Container>
+        </>
     );
 };
+
+PublicacionesUsuario.defaultProps = {
+    publicaciones: []
+}
+
+PublicacionesUsuario.propTypes = {
+    publicaciones: PropTypes.array
+}
 
 export default PublicacionesUsuario;
