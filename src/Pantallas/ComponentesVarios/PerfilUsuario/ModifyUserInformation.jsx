1f30883ef73dfc4232960f478f0f8b133a167106
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-12-08 17:40:42 -0600

    Implementacion funcional de BasicButton en componente ModifyUserInformation.

--- src/Pantallas/ComponentesVarios/PerfilUsuario/ModifyUserInformation.jsx
+++ src/Pantallas/ComponentesVarios/PerfilUsuario/ModifyUserInformation.jsx
@@ -1,7 +1,23 @@
-const ModifyUserInformation = () => {
-    return (
-        <></>
-    );
+import { Container } from "react-bootstrap";
+import PropTypes from "prop-types";
+import BasicButton from "./Botones/BasicButton";
+
+const ModifyUserInformation = ( props ) => {
+    if( props.id_usuario === localStorage.getItem( "KeyID" ) ) {
+        return (
+            <Container>
+                <BasicButton content="Editar Datos"/>
+            </Container>
+        );
+    }
 };
+
+ModifyUserInformation.defaultProps = {
+    id_usuario: ''
+}
+
+ModifyUserInformation.propTypes = {
+    id_usuario: PropTypes.number
+}
 
 export default ModifyUserInformation;
