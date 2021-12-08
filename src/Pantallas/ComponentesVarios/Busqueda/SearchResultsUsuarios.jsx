e3af675b004272a8e71411fed733596bedfc51f1
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-12-07 20:15:27 -0600

    Se cambiaron estilos del componente Busqueda.

--- src/Pantallas/ComponentesVarios/Busqueda/SearchResultsUsuarios.jsx
+++ src/Pantallas/ComponentesVarios/Busqueda/SearchResultsUsuarios.jsx
@@ -1,4 +1,4 @@
-import { Container, ListGroup, Form } from "react-bootstrap";
+import { Container, ListGroup, Form, Row, Col, Nav } from "react-bootstrap";
 import PropTypes from "prop-types";
 
 const SearchResultsUsuarios = ( props ) => {
@@ -8,13 +8,21 @@ const SearchResultsUsuarios = ( props ) => {
                 <Form.Label>Usuarios:</Form.Label>
                 <ListGroup id="UsuariosList" as="ul">
                     { props.usuarios.map( ( usuario, index ) => (
-                        <ListGroup.Item className="ListItem" action href="http://localhost:3000/Home" as="li">
-                            <Container className="FotoPerfilUsuario"></Container>
-                            <Container className="DetallesUsuario">
-                                <Container className="NombreUsuario">
-                                    <h5>{ usuario.nombre_usuario }</h5>
-                                </Container>
-                            </Container>
+                        <ListGroup.Item className="ListItem" as="li">
+                            <Nav.Link className="ListLink" href="/Home">
+                                <Row className="ListItemRow">
+                                    <Col className="IconColumn" sm={ 1 }>
+                                        <Container className="FotoPerfilUsuario"></Container>
+                                    </Col>
+                                    <Col className="DetailsColumn" sm={ 11 }>
+                                        <Container className="DetallesUsuario">
+                                            <Container className="NombreUsuario">
+                                                <h5>{ usuario.nombre_usuario }</h5>
+                                            </Container>
+                                        </Container>
+                                    </Col>
+                                </Row>
+                            </Nav.Link>
                         </ListGroup.Item>
                     ) ) }
                 </ListGroup>
