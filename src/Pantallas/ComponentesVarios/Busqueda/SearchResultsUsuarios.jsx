f32f035501bbf080549dfc19b4b3808726c57ac7
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-12-09 17:11:59 -0600

    Se modifico estilos de componente UserImage y del contenedor que contiene UserImage.

--- src/Pantallas/ComponentesVarios/Busqueda/SearchResultsUsuarios.jsx
+++ src/Pantallas/ComponentesVarios/Busqueda/SearchResultsUsuarios.jsx
@@ -1,5 +1,6 @@
 import { Container, ListGroup, Form, Row, Col, Nav } from "react-bootstrap";
 import PropTypes from "prop-types";
+import UserImage from "./UserImage";
 
 const SearchResultsUsuarios = ( props ) => {
     return (
@@ -8,11 +9,13 @@ const SearchResultsUsuarios = ( props ) => {
                 <Form.Label>Usuarios:</Form.Label>
                 <ListGroup id="UsuariosList" as="ul">
                     { props.usuarios.map( ( usuario, index ) => (
-                        <ListGroup.Item className="ListItem" as="li">
+                        <ListGroup.Item key={ index } className="ListItem" as="li">
                             <Nav.Link className="ListLink" href="/Home">
                                 <Row className="ListItemRow">
                                     <Col className="IconColumn" sm={ 2 }>
-                                        <Container className="FotoPerfilUsuario"></Container>
+                                        <Container className="FotoPerfilUsuario">
+                                            <UserImage fotoPerfil={ usuario.foto_perfil }/>
+                                        </Container>
                                     </Col>
                                     <Col className="DetailsColumn" sm={ 10 }>
                                         <Container className="DetallesUsuario">
