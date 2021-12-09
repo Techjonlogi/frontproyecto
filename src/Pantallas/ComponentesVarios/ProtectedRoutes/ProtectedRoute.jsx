4f44ae5776a886fd88ea7804b05b2bf64afa9671
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-12-09 14:06:12 -0600

    Implementacion basica de funcionalidad para prevenir que un usuario acceda a una ruta protegida.

--- src/Pantallas/ComponentesVarios/ProtectedRoutes/ProtectedRoute.jsx
+++ src/Pantallas/ComponentesVarios/ProtectedRoutes/ProtectedRoute.jsx
@@ -1,5 +1,16 @@
-const ProtectedRoute = () => {
+import { Route, Redirect } from "react-router-dom";
 
+const ProtectedRoute = ( { isAuth: isAuth, component: Component, ...rest } ) => {
+    return (
+        <Route { ...rest } render={ () => {
+            if( isAuth ) {
+                return <Component />;
+            }
+            return (
+                <Redirect to={ { pathname: "/Home", state: { from: props.location } } }/>
+            );
+        } }></Route>
+    );
 };
 
 export default ProtectedRoute;
