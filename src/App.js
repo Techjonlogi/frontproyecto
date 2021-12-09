2e1dcdfcefe362c01ca3c88572c924a167f362fa
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-12-09 14:14:08 -0600

    Reestructuracion de rutas especificadas en App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import './App.css';
-
+import ProtectedRoute from './Pantallas/ComponentesVarios/ProtectedRoutes/ProtectedRoute';
 import Login from './Pantallas/Login/Login'
 import Home from './Pantallas/Home/Home';
 import Error404 from './Pantallas/PaginaError/Error404';
@@ -11,20 +11,18 @@ import UserRegistry from './Pantallas/UserRegistry/UserRegistry';
 import Busqueda from './Pantallas/Busqueda/Busqueda';
 import VerpublicacionEspecifica from './Pantallas/VerPublicacionEspecifica/VerPublicacionSeleccionada';
 
-
-
 function App() {
   return (
    <BrowserRouter>
    <Routes>
+     <Route exact path="/" element={ <Home/> }></Route>
+     <Route exact path="Home" element={ <Home/> }></Route>
      <Route exact path="Login" element={ <Login/> }></Route>
      <Route exact path="Register" element={ <UserRegistry/> }></Route>
-     <Route exact path="Home" element={ <Home/> }></Route>
-     <Route exact path="Publicar" element={ <CrearPublicacion /> }></Route>
-     <Route exact path="Perfil/:id_usuario" element={ <PerfilUsuario /> }></Route>
-     <Route exact path="/" element={ <Home/> }></Route>
      <Route exact path="Busqueda/:search_query" element={ <Busqueda/> }></Route>
-     <Route exact path ="PublicacionSeleccionada/:idPublicacion" element ={<VerpublicacionEspecifica/>}></Route> 
+     <Route exact path="Perfil/:id_usuario" element={ <PerfilUsuario /> }></Route>
+     <Route exact path="PublicacionSeleccionada/:idPublicacion" element ={<VerpublicacionEspecifica/>}></Route> 
+     <Route exact path="Publicar" element={ <CrearPublicacion /> }></Route>
      <Route path="*" element={ <Error404/> }></Route>
    </Routes>
    </BrowserRouter>
