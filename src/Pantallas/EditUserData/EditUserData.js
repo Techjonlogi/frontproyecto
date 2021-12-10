dff6c00ce526ba0ed4a04b9d2c975cf76011cba3
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-12-10 13:47:54 -0600

    Avance de implementacion del componente EditUserData.

--- src/Pantallas/EditUserData/EditUserData.js
+++ src/Pantallas/EditUserData/EditUserData.js
@@ -1,9 +1,43 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './EditUserData.css';
+import { Formik } from "formik";
+import { Container, Form, Col, Row } from "react-bootstrap";
+import { ConvertToBase64 } from "../ComponentesVarios/Utilidades/UtilityFunctions";
+import Api from "../ComponentesVarios/Utilidades/Api/Api";
+import ConfigWithAuth from "../ComponentesVarios/Utilidades/Api/Configurations/ConfigWithAuth";
+import Endpoints from "../ComponentesVarios/Utilidades/Api/ApiEndpoints";
+import NavBar from "../ComponentesVarios/BarraNavegacion/NavBar";
 
 const EditUserData = () => {
     return (
-        <></>
+        <Formik
+            initialValues= { {
+                nombres: '',
+                apellidos: '',
+                nombre_usuario: '',
+                correo_electronico: '',
+                contrasena: '',
+                confirmar_contrasena: ''
+            } }
+        >
+        { formik => (
+            <>
+                <Navbar />
+                <Container className="EditUserDataContent">
+                    <Row className="BodyRow">
+                        <Col id="EmptyLeftColumn"></Col>
+                        <Col id="EditUserDataColumn">
+                            <Container id="FormContainer">
+                                <Form id="EditUserDataForm" onSubmit={ formik.handleSubmit }>
+
+                                </Form>
+                            </Container>
+                        </Col>
+                        <Col id="EmptyRightColumn"></Col>
+                    </Row>
+                </Container>
+            </>
+        ) }</Formik>
     );
 };
 
