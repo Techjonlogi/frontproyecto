e1021ecfa7aeb9bff50c0fec6458d9c344b893e3
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-12-09 18:03:40 -0600

    Modificacion de regex utilizada para validar la contrasena introducida por el usuario.

--- src/Pantallas/ComponentesVarios/Utilidades/Regex.js
+++ src/Pantallas/ComponentesVarios/Utilidades/Regex.js
@@ -3,7 +3,7 @@ const Expresiones = {
     nombreExpresion: /^[a-zA-ZÀ-ÿ\s]{3,40}$/, // Letras y espacios, pueden llevar acentos.
     descripcionExpresion: /^[a-zA-ZÀ-ÿ\s\.]{3,200}$/, // Letras y espacios, pueden llevar acentos.
     expresionGeneral: /^[a-zA-ZÀ-ÿ0-9\_\-\s]{3,40}$/, //Letras, numeros, guion, guion bajo y espacios.
-    passwordExpresion: /^.{4,20}$/, // 4 a 12 digitos.
+    passwordExpresion: /^[a-zA-Z0-9\_\-]{4,20}$/, // 4 a 12 digitos.
     correoExpresion: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
     telefonoExpresion: /^\d{7,14}$/ // 7 a 14 numeros.
 };
