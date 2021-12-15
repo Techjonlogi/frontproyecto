da05b3314d9b35e56823a74a5749aa6764a4e8b2
Avival <31579334+Avival-Code@users.noreply.github.com>
2021-12-14 20:25:21 -0600

    Limpieza de codigo de componente Home y VerPublicacionSeleccionada

--- src/Pantallas/Home/Home.js
+++ src/Pantallas/Home/Home.js
@@ -1,66 +1,49 @@
-import React, { useState,useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import './Home.css';
+import { useState,useEffect } from "react";
+import { Container } from "react-bootstrap";
 import Api from "../ComponentesVarios/Utilidades/Api/Api";
 import Endpoints from "../ComponentesVarios/Utilidades/Api/ApiEndpoints";
-import { Container } from "react-bootstrap";
 import NavBar from "../ComponentesVarios/BarraNavegacion/NavBar";
 import CardPublicacion from "../ComponentesVarios/CardParaPublicacion/CardPublicacion";
 
 const Home = () => { 
-
-  const [publicacionesEstado , setPublicaciones] = useState([]);
+  const [ publicacionesEstado , setPublicaciones ] = useState( [] );
 
   const LlenarArreglos = async () => {
     let publicaciones = [];
 
-    await Api.get(Endpoints.publicaciones)
-      .then((res) => {
+    await Api.get( Endpoints.publicaciones )
+      .then( ( res ) => {
         publicaciones = res.data;
-      })
-      .catch((e) =>
-        alert("ocurrio algun error al recuperar las publicaciones")
+      } ).catch( ( e ) =>
+        alert( "ocurrio algun error al recuperar las publicaciones" )
       );
 
-    await publicaciones.forEach((publicacion) => {
-
-
-      
-      Api.get(
-        Endpoints.multimedia +
-          "/" +
-          publicacion.clave_publicacion +
-          Endpoints.obtenerMultimediaEspecifica
-      )
-        .then((res) => {
-          publicacion.multimedia = res.data.multimedia;
-
-          setPublicaciones(publicacionesEstado=>[...publicacionesEstado, publicacion]);
-          
-        })
-        .catch((e) => alert("Ocurrio un error al cargar las imagenes"));
+    await publicaciones.forEach( ( publicacion ) => {
+        Api.get(
+        Endpoints.multimedia + "/" + publicacion.clave_publicacion + Endpoints.obtenerMultimediaEspecifica
+      ).then( ( res ) => {
+        publicacion.multimedia = res.data.multimedia;
+        setPublicaciones( publicacionesEstado=>[ ...publicacionesEstado, publicacion ] );
+      } ).catch( ( e ) => alert( "Ocurrio un error al cargar las imagenes" ) );
     });
-
   };
 
-  useEffect(() => {
+  useEffect( () => {
     LlenarArreglos();
-  }, []);
+  }, [] );
 
   return (    
     <div className="PrincipalDiv">
       <NavBar />
       <Container className="containerPublicaciones">
-
-      
-      
-        {publicacionesEstado.map(publicacion =>(
+        { publicacionesEstado.map( publicacion => (
             <div>
-              <CardPublicacion key={publicacion.clave_publicacion} publication = {publicacion}/>
+              <CardPublicacion key={ publicacion.clave_publicacion } publication={ publicacion }/>
             </div>
-        ))  }
+        ) ) }
       </Container>
-      
     </div>
   );
 };
